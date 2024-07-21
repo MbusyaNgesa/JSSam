60d24b3d75de059560fcff580619a4cd3ddff029
MbusyaNgesa <theengesa@gmail.com>
2024-07-21 17:35:30 +0300

    String Functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 function reverse(str){
 
- return str.split("").reverse().join("");
+ return str.split("").reverse().join("");//split(), separates string into substring
 }
 console.log(reverse("Ngesa"));
 
@@ -13,7 +13,20 @@ function reverse1(str1){
     return str1.split("").reverse().toString();
 }
 console.log(reverse1('Daphine'));
+
+
 // Count Characters: Create a function that counts the number
 // of characters in a string.
+
+function count(str){
+    return str.length;
+}
+console.log(count("What is going on"));
+
 // Capitalize Words: Implement a function that capitalizes the
 // first letter of each word in a sentence.
+
+function capital(alph){
+    return alph.charAt(0).toUpperCase() + alph.slice(1);
+}
+console.log(capital("welcome"));
